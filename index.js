f8114106b4df8a7792dab687dd5c0c40b2f08766
Ricardo Menotti <ricardomenotti31@gmail.com>
2024-02-03 21:24:04 -0500

    done

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const qrcode = require('qrcode-terminal');
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const express = require("express");
+
+require("dotenv").config();
 const whatsapp = new Client({
   authStrategy: new LocalAuth(),
+  puppeteer: {
+    args: ["--no-sandbox"],
+  },
 });
 
-const express = require('express');
 const app = express();
 
 app.use(express.json()); // for parsing application/json
@@ -12,38 +17,56 @@ app.use(express.urlencoded({ extended: true }));
 const port = 3000;
 
 // whatsapp
-whatsapp.on('qr', (qr) => {
+whatsapp.on("qr", (qr) => {
   qrcode.generate(qr, {
     small: true,
   });
 });
 
-whatsapp.on('ready', () => {
-  console.log('Client is ready!');
+whatsapp.on("ready", () => {
+  console.log("Client is ready!");
 });
 
-whatsapp.on('message', async (message) => {
-  if (message.body === '!ping') {
-    message.reply('pong');
-    console.log(message.from);
+whatsapp.on("message", async (message) => {
+  if (message.body === "!ping") {
+    message.reply("pong");
   }
 });
 
-app.post('/send-whatsapp', async (req, res) => {
-  const { phoneNumber, message } = req.body;
+app.get("/health", (req, res) => {
+  return res.send("ok");
+});
+app.post("/send-whatsapp", async (req, res) => {
+  if (process.env.CRON_API_KEY !== req.headers.authorization) {
+    console.log("unauthorized", req.headers.authorization, process.env.CRON_API_KEY);
+    return res.send("invalid request");
+  }
 
+  const message = req.body.message;
+  let phoneNumber = req.body.phoneNumber;
+
+  if (!phoneNumber || !message) {
+    console.log("not message nor number");
+    res.send("invalid request");
+    return;
+  }
+
+  if (phoneNumber.includes("+")) {
+    phoneNumber = phoneNumber.split("+")[1];
+  }
+
+  const numberDetails = await whatsapp.getNumberId(phoneNumber);
+
+  if (!numberDetails) {
+    console.log("no details");
+    return res.send("invalid request");
+  }
   try {
-    if (!phoneNumber || !message) {
-      res.send('invalid request');
-      return;
-    }
-
-    whatsapp.sendMessage(phoneNumber, message);
+    whatsapp.sendMessage(numberDetails._serialized, message);
   } catch (error) {
-    console.log(error);
-    res.send('hola');
+    res.send("hola");
   }
-  res.send('hola');
+  res.send("Done");
 });
 
 whatsapp.initialize();
