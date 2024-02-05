a3e5597196d0d635ea6446bde0fb07b4013a1b55
ricardomenotti <ricardomenotti31@gmail.com>
2024-02-05 10:18:15 -0500

    crons added

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const qrcode = require("qrcode-terminal");
-const { Client, LocalAuth } = require("whatsapp-web.js");
-const express = require("express");
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const express = require('express');
+const cron = require('node-cron');
 
-require("dotenv").config();
+require('dotenv').config();
 const whatsapp = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    args: ["--no-sandbox"],
+    args: ['--no-sandbox'],
   },
 });
 
@@ -14,62 +15,108 @@ const app = express();
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true }));
-const port = 3000;
 
 // whatsapp
-whatsapp.on("qr", (qr) => {
+whatsapp.on('qr', (qr) => {
   qrcode.generate(qr, {
     small: true,
   });
 });
 
-whatsapp.on("ready", () => {
-  console.log("Client is ready!");
+whatsapp.on('ready', () => {
+  console.log('Client is ready!');
 });
 
-whatsapp.on("message", async (message) => {
-  if (message.body === "!ping") {
-    message.reply("pong");
+whatsapp.on('message', async (message) => {
+  if (message.body === '!ping') {
+    message.reply('pong');
   }
 });
 
-app.get("/health", (req, res) => {
-  return res.send("ok");
+// routes
+app.get('/health', (req, res) => {
+  return res.send('ok');
 });
-app.post("/send-whatsapp", async (req, res) => {
+app.post('/send-whatsapp', async (req, res) => {
   if (process.env.CRON_API_KEY !== req.headers.authorization) {
-    console.log("unauthorized");
-    return res.send("invalid request");
+    console.log('unauthorized');
+    return res.send('invalid request');
   }
 
   const message = req.body.message;
   let phoneNumber = req.body.phoneNumber;
 
   if (!phoneNumber || !message) {
-    console.log("not message nor number", phoneNumber, message);
-    res.send("invalid request");
+    console.log('not message nor number', phoneNumber, message);
+    res.send('invalid request');
     return;
   }
 
-  if (phoneNumber.includes("+")) {
-    console.log('phone includes', phoneNumber)
-    phoneNumber = phoneNumber.split("+")[1];
+  if (phoneNumber.includes('+')) {
+    console.log('phone includes', phoneNumber);
+    phoneNumber = phoneNumber.split('+')[1];
   }
 
   const numberDetails = await whatsapp.getNumberId(phoneNumber);
 
   if (!numberDetails) {
-    console.log("no details");
-    return res.send("invalid request");
+    console.log('no details');
+    return res.send('invalid request');
   }
   try {
-    console.log('sending message to>>>', numberDetails._serialized, message)
+    console.log('sending message to>>>', numberDetails._serialized, message);
     whatsapp.sendMessage(numberDetails._serialized, message);
   } catch (error) {
-    res.send("hola");
+    res.send('hola');
   }
-  res.send("Done");
+  res.send('Done');
 });
+
+// cron jobs
+
+const whatsappReminderJob = cron.schedule('*/5 * * * *', async () => {
+  const url = process.env.CRON_WHATSAPP_URL;
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      authorization: process.env.CRON_API_KEY,
+    },
+  });
+});
+
+const smsReminderJob = cron.schedule('*/5 * * * *', async () => {
+  const url = process.env.CRON_SMS_URL;
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      authorization: process.env.CRON_API_KEY,
+    },
+  });
+});
+
+const emailReminderJob = cron.schedule('*/5 * * * *', async () => {
+  const url = process.env.CRON_EMAIL_URL;
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      authorization: process.env.CRON_API_KEY,
+    },
+  });
+});
+
+
+const webhooksReminderJob = cron.schedule('*/5 * * * *', async () => {
+  const url = process.env.CRON_WEBHOOK_URL;
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      authorization: process.env.CRON_API_KEY,
+    },
+  });
+});
+
+// init
+const port = 3000;
 
 whatsapp.initialize();
 
